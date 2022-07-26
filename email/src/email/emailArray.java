ee8c0f8d1054507ac2a277ed90f0c0017b9aedb3
kirankumar290200 <68763255+kirankumar290200@users.noreply.github.com>
2022-07-26 16:40:46 +0530

    Add files via upload

---
+++ email/src/email/emailArray.java
@@ -0,0 +1,33 @@
+package email;
+
+import java.util.Scanner;
+public class emailArray {
+	
+    public static void main(String[] args) 
+    { 
+      String[] strArray = new String[6];
+      strArray[0]= "123@gmail.com";
+      strArray[1]= "abc@gmail.com"; 
+      strArray[2]= "xyz@gmail.com"; 
+      strArray[3]= "abcd@gmail.com"; 
+      strArray[4]= "345@gmail.com";
+      strArray[5]= "583@gmail.com"; 
+      
+        boolean found = false;
+        int index = 0;
+        
+        Scanner Str=new Scanner(System.in);
+        System.out.println("Enter the Email ID:");
+        String a= Str.nextLine(); 
+       for (int i = 0; i <strArray.length; i++) {
+       if(a.equals(strArray[i])) {
+            index = i; found = true; 
+            }
+        }
+       if(found)
+          System.out.println("\n" +a +" found at the index "+index);
+        else
+          System.out.println("\n" +a +" not found in the array");
+ 
+    }
+}
